6bacb863b3e7f49945c05e56f7b6a2b02a1d2a46
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-01-31 17:18:42 +0100

    create the movie card

--- src/App.js
+++ src/App.js
@@ -3,6 +3,13 @@ import './App.css';
 import SearchIcon from "./search.svg";
 
 const API_URL = 'https://www.omdbapi.com/?apikey=8e92b44d';
+const movie = {
+    "Title": "The Batman",
+    "Year": "2022",
+    "imdbID": "tt1877830",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMDdmMTBiNTYtMDIzNi00NGVlLWIzMDYtZTk3MTQ3NGQxZGEwXkEyXkFqcGdeQXVyMzMwOTU5MDk@._V1_SX300.jpg"
+}
 
 const App = () => {
     useEffect(() => {
@@ -23,13 +30,28 @@ const App = () => {
                     type="text"
                     placeholder="Search for a movie..."
                     value="batman"
-                    onChange={() => {}}
+                    onChange={() => {
+                    }}
                 />
                 <img
                     src={SearchIcon}
                     alt="search-icon"
                     onClick={() => searchMovies('batman')}
                 />
+            </div>
+            <div className={"container"}>
+                <div className="movie">
+                    <div>
+                        <p>{movie.Year}</p>
+                    </div>
+                    <div>
+                        <img src={movie.Poster === 'N/A' ? 'https://via.placeholder.com/400' : movie.Poster} alt={movie.Title} />
+                    </div>
+                    <div>
+                        <span>{movie.Type}</span>
+                        <h3>{movie.Title}</h3>
+                    </div>
+                </div>
             </div>
         </div>
     );
