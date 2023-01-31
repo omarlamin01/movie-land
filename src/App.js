d957676cb16db5dd9f1d509cd5f9983679816d1b
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-01-31 16:36:21 +0100

    search for movies

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-const App = () => {
-  return (
-    <div className="App">
+import {useEffect} from "react";
 
-    </div>
-  );
+const API_URL = 'https://www.omdbapi.com/?apikey=8e92b44d';
+
+const App = () => {
+    useEffect(() => {
+
+    }, []);
+
+    const searchMovies = async (searchTerm) => {
+        const response = await fetch(`${API_URL}&s=${searchTerm}`);
+        const data = await response.json();
+        console.log(data);
+    }
+
+    return (
+        <div className="App">
+
+        </div>
+    );
 }
 
 export default App;
