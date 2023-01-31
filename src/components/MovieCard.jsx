9bee6269755e1865d3b1897a030fbce243c30fd1
omar lamine <76263000+omarlamin01@users.noreply.github.com>
2023-01-31 17:29:29 +0100

    Update MovieCard.jsx

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,20 @@
+
+
+const MovieCard = ({ movie }) => {
+    return (
+        <div className="movie">
+            <div>
+                <p>{movie.Year}</p>
+            </div>
+            <div>
+                <img src={movie.Poster === 'N/A' ? 'https://via.placeholder.com/400' : movie.Poster} alt={movie.Title} />
+            </div>
+            <div>
+                <span>{movie.Type}</span>
+                <h3>{movie.Title}</h3>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard;
